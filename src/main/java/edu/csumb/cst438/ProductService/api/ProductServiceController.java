04c2eb55b08f7c0472757d5dafb4cf94bb4be3cc
Josh Saavedra <jsaavedra@csumb.edu>
2018-12-01 16:55:41 -0800

    Minor change

--- src/main/java/edu/csumb/cst438/ProductService/api/ProductServiceController.java
+++ src/main/java/edu/csumb/cst438/ProductService/api/ProductServiceController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import edu.csumb.cst438.ProductService.api.products.*;
-// import edu.csumb.cst438.ProductService.business.Manager;
+import edu.csumb.cst438.ProductService.business.Manager;
 
 @RestController
 public class ProductServiceController {
     @Autowired
-    // Manager manager;
+    Manager manager;
 
 
     @GetMapping ("/Products")
